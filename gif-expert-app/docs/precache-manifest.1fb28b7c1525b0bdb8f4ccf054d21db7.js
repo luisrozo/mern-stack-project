ca18ebfbe51ca54d48d2a87e21a4d3a318d1ebc6
luisrozo <luisgonzagarozo@gmail.com>
2020-10-17 10:40:04 +0200

    add docs folder

---
+++ gif-expert-app/docs/precache-manifest.1fb28b7c1525b0bdb8f4ccf054d21db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cad324b1c24c701caf67b28a4996864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91745d4603523689a29",
+    "url": "/static/css/main.40a07fb7.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91745d4603523689a29",
+    "url": "/static/js/main.a85c8e7f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
--- gif-expert-app/src/GifExpertApp.js
+++ gif-expert-app/src/GifExpertApp.js
@@ -4,7 +4,7 @@ import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
     
-    const [ categories, setCategories ] = useState(['Pokemon']);
+    const [ categories, setCategories ] = useState(['Naruto']);
 
     return (
         <>
